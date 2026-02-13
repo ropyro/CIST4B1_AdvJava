958eb3212465049937c50a03b9eb18264faa2d94
ropyro <roninrichman@gmail.com>
2026-02-12 21:13:16 -0800

    In class practice trying to implement insertion sort

---
+++ participation/SortingPractice.java
@@ -0,0 +1,24 @@
+
+
+
+public class SortingPractice {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void insertionSort(int[] arr){
+        for (int i = 1; i < arr.length - 1; i++) {
+            for (int j = i; j > 0; j--) {
+                if (arr[j] > ) {
+
+                }
+            }
+
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+
+}
