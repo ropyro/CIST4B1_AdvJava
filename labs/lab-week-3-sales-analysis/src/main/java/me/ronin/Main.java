298d628ff403620c1fca9a0f5b9e39ec6734248f
ropyro <roninrichman@gmail.com>
2026-02-15 21:46:56 -0800

    Source list updating for where im getting inspiration

--- labs/lab-week-3-sales-analysis/src/main/java/me/ronin/Main.java
+++ labs/lab-week-3-sales-analysis/src/main/java/me/ronin/Main.java
@@ -1,7 +1,21 @@
 package me.ronin;
 
-public class Main {
-    public static void main(String[] args) {
+import me.ronin.utils.DataHelper;
 
+import java.io.IOException;
+
+
+/**
+ * @author roninrichman
+ *
+ * Sources:
+ * LocalDate class help: https://stackoverflow.com/questions/3985392/generate-random-date-of-birth
+ * Really interesting video about the JIT and hacky ways to warm up code for benchmarking: https://www.youtube.com/watch?v=7af_QJiLWHI
+ *
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        DataHelper.generateRecordsCSV(100, "TestFile");
     }
 }
