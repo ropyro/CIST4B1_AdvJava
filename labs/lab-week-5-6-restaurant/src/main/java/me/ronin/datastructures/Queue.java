c89368d2339b3e104e15742fa6a1d41261d690bb
ropyro <roninrichman@gmail.com>
2026-03-10 22:31:28 -0700

    Restaurant Sim Lab Week 5&6 initial commit

---
+++ labs/lab-week-5-6-restaurant/src/main/java/me/ronin/datastructures/Queue.java
@@ -0,0 +1,45 @@
+package me.ronin.datastructures;
+
+public class Queue<T> {
+    private Object[] data;
+    private int size;
+    private int head, tail;
+
+    public Queue(int size) {
+        this.data = new Object[size];
+        this.size = 0;
+        head = 0;
+        tail = 0;
+    }
+
+    public void enqueue(T item) {
+        if (this.size == this.data.length) {
+            Object[] newData = new Object[this.size * 2];
+            for (int i = 0; i < this.size; i++) {
+                newData[i] = this.data[i];
+            }
+            newData[size] = item;
+        } else {
+            this.data[this.tail] = item;
+            this.tail = (this.tail + 1) % data.length;
+            this.size++;
+        }
+    }
+
+    public T dequeue() {
+        if (size == 0) {
+            System.out.println("Error");
+            return null;
+        } else {
+            T retValue = (T) this.data[this.head];
+            this.data[this.head] = null;
+            this.head = (this.head + 1) % data.length;
+            this.size--;
+            return retValue;
+        }
+    }
+
+    public int size(){
+        return size;
+    }
+}
