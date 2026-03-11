f83c61ee6c4b33e765a863e5bcac3d1e8159d422
ropyro <roninrichman@gmail.com>
2026-03-10 22:30:15 -0700

    Hash table + maps lesson

---
+++ participation2/src/main/java/me/ronin/week6/HTEntry.java
@@ -0,0 +1,12 @@
+package me.ronin.week6;
+
+public class HTEntry {
+
+    public String key;
+    public Object value;
+
+    public HTEntry(String key, Object value){
+        this.key = key;
+        this.value = value;
+    }
+}
