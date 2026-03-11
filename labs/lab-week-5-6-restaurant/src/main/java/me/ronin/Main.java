e9ea27c7baf4dee83730c0d252ab0f3b1e351dd0
ropyro <roninrichman@gmail.com>
2026-03-10 22:32:53 -0700

    Week 4 stuff

---
+++ labs/lab-week-5-6-restaurant/src/main/java/me/ronin/Main.java
@@ -0,0 +1,10 @@
+package me.ronin;
+
+import me.ronin.frontofhouse.Restaurant;
+
+public class Main {
+    public static void main(String[] args) {
+        Restaurant simulationRestie = new Restaurant(20);
+        simulationRestie.open();
+    }
+}
